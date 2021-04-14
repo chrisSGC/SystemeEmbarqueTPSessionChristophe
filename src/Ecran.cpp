cadd430209f300ed05c2e2b858f388ca50e9f441
Christophe Ferru <1941676@cegeprdl.ca>
2021-04-14 19:57:09 -0400

    debut des commentaires

--- src/Ecran.cpp
+++ src/Ecran.cpp
@@ -1,11 +1,22 @@
+/**
+    Classe qui permet de gérer l'écran DEL du projet
+    @file ecran.cpp
+    @author Christophe Ferru
+    @version 1.0 - 14 Avril 2021                           
+**/
 #include <Arduino.h>
 #include "Ecran.h"
 
+/**
+ * Methode qui met la configuration de l'écran en place
+ * 
+ * @param addrI2C Adresse I2C de l'écran DEL
+ * @return 1 en cas d'erreur | 0 si tout va bien
+ **/
 int Ecran::lancement(uint8_t addrI2C) {
     if(afficheur.begin(SSD1306_SWITCHCAPVCC, addrI2C)){
         afficheur.cp437(true);
-        afficheur.clearDisplay();
-        afficheur.setTextColor(SSD1306_WHITE);
+        EffacerEcran(); // On appele la fonction EffacerEcran() pour effacer l'écran
 
         return(0); // Initialisation effectuée
     }else{
@@ -14,17 +25,34 @@ int Ecran::lancement(uint8_t addrI2C) {
 
 }
 
-void Ecran::MettreEnVeille(){
+/**
+ * Methode qui permet d'effacer le contenu affiché à l'écran
+ * 
+ * @return void
+ **/ 
+void Ecran::EffacerEcran(){
     afficheur.clearDisplay();
 }
 
+/**
+ * 
+ * 
+ * @param hauteur int qui determine la hauteur à laquelle se trouve la ligne
+ * @return void
+ **/ 
 void Ecran::InitialiserAffichage(int hauteur){
-    MettreEnVeille();
+    EffacerEcran(); // On appele la fonction EffacerEcran() pour effacer l'écran
     afficheur.setTextSize(1);
     afficheur.setTextColor(SSD1306_WHITE);
     afficheur.setCursor(0, hauteur);
 }
 
+/**
+ * 
+ * 
+ * @param nbrCharSaisis nombre de caractères saisis par l'utilisateur afin d'afficher le bon nombe d'étoiles
+ * @return void 
+ **/
 void Ecran::AfficherSaisie(byte nbrCharSaisis){
     byte nbrLocal = 0;
     String affichageCode = "";
